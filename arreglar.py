258dd7483cb04e56d911edc28a8de83bdb6bb087
MainDavis <44068483+MainDavis@users.noreply.github.com>
2023-01-27 18:11:05 +0100

    ARREGLADO

--- arreglar.py
+++
@@ -1,9 +0,0 @@
-# Leo el archivo ip_subdominos.txt y borro las filas vacias
-
-with open('ip_subdominios.txt', 'r') as f:
-    ip_subdominos = f.read().splitlines()
-
-with open('ip_subdominios.txt', 'w') as f:
-    for ip_subdominio in ip_subdominos:
-        if ip_subdominio != '':
-            f.write(ip_subdominio + '\n')
